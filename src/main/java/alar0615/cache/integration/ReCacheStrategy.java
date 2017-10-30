6cd7b5464a3f36c22d5328c82f26015ccf2b8e98
alar0615 <salierudei@gmail.com>
2017-10-30 11:24:06 +0400

    removed unused classes

--- src/main/java/alar0615/cache/integration/ReCacheStrategy.java
+++
@@ -1,9 +0,0 @@
-package alar0615.cache.integration;
-
-/**
- * Created with IntelliJ IDEA.
- * User: alar0615
- */
-public interface ReCacheStrategy<K, V> {
-    public boolean isNeedReCache(ILeveledCache<K, V> leveledCache);
-}
